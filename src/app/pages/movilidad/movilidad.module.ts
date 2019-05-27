af12d23f4367bd99fe47270ea8811d4b54e332a3
BOTOOM <eddiazr@correo.udistrital.edu.co>
2019-05-27 13:12:13 -0500

    se prueba conexion con el mid usando el metodo de getconvenio #5

--- src/app/pages/movilidad/movilidad.module.ts
+++ src/app/pages/movilidad/movilidad.module.ts
@@ -2,6 +2,7 @@ import { MovilidadRoutingModule, routedComponents } from './movilidad-routing.mo
 import { NgModule } from '@angular/core';
 import { ThemeModule } from '../../@theme/theme.module';
 import { MovilidadAcademicaService } from '../../@core/data/movilidad_academica.service';
+import { MovilidadMidService } from '../../@core/data/movilidad_mid.service';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
 import { ToasterModule } from 'angular2-toaster';
 import { SharedModule } from '../../shared/shared.module';
@@ -21,6 +22,7 @@ import { ToasterService} from 'angular2-toaster';
   ],
   providers: [
     MovilidadAcademicaService,
+    MovilidadMidService,
     ToasterService,
   ],
   exports: [
